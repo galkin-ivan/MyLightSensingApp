da0543b43fb221c2cf10b3b113546314476509a0
galkinivanMac <galkinivansergeevich@gmail.com>
2017-09-28 12:20:10 +0300

    Сделал тупо вывод данных на экран

---
+++ app/src/main/cpp/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Иван Галкин on 20.09.2017.
+//
+#include "stdio.h"
+#include "jni.h"
+
+void Java_ru_galkinivan_mylightsensingapp_MainActivity_main(){
+    printf("lol i am cpp)))))");
+}
--- app/src/main/java/ru/galkinivan/mylightsensingapp/MainActivity.java
+++ app/src/main/java/ru/galkinivan/mylightsensingapp/MainActivity.java
@@ -7,16 +7,38 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.method.MovementMethod;
+import android.text.method.ScrollingMovementMethod;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager sensorManager;
     private Sensor lightSensor;
+    private Integer friquancy;
+    private double timeIntervalDouble;
+    private Integer timeInterval;
+    private Integer solvedPowerOfTwo;
+    private Integer numberOfValuesRegistred;
     //UI vars
     private Button startButton;
     private Button stopButton;
+    private TextView friquancyTextView;
+    private TextView powerOfTwoTextView;
+    private TextView resultTextView;
+
+
+    static {
+        //System.loadLibrary("main");
+    }
+    native void main();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,16 +47,51 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        //UI asinings
+        //UI assigning
         startButton = (Button) findViewById(R.id.buttonStartLightSensing);
         stopButton = (Button) findViewById(R.id.buttonStopLightSensing);
+        friquancyTextView = (TextView) findViewById(R.id.editTextFrequency);
+        powerOfTwoTextView = (TextView) findViewById(R.id.editTextPowerOfTwo);
+        resultTextView = (TextView) findViewById(R.id.resultTextView);
 
     }
 
     private void startSensing(View v){
-        startButton.setVisibility(View.GONE);
-        stopButton.setVisibility(View.VISIBLE);
-        sensorManager.registerListener(this, lightSensor, 1000000);
+        boolean frCheck = false;
+        boolean potCheck = false;
+        // get and check values
+        if(!"".equals(friquancyTextView.getText().toString())) {
+            friquancy = Integer.parseInt(friquancyTextView.getText().toString());
+            if(friquancy < 1000000 && friquancy > 0) {
+                frCheck = true;
+                timeIntervalDouble = 1000000.0 / friquancy;
+                timeInterval = Double.valueOf(Math.ceil(timeIntervalDouble)).intValue(); // losing some resolution...
+                System.out.println("Set frrquancy is: " + friquancy + " and time interval is: " + timeInterval);
+            }
+            else
+                friquancyTextView.setError(getResources().getString(R.string.badValueError));
+        }
+        else
+            friquancyTextView.setError(getResources().getString(R.string.requaredFeildError));
+
+        if(!"".equals(powerOfTwoTextView.getText().toString())) {
+            potCheck = true;
+            solvedPowerOfTwo = Double.valueOf(Math.pow(2, Double.parseDouble(powerOfTwoTextView.getText().toString()))).intValue();
+        }
+        else
+            powerOfTwoTextView.setError(getResources().getString(R.string.requaredFeildError));
+
+        // run routine if all good
+        if (frCheck && potCheck) {
+            startButton.setVisibility(View.GONE);
+            stopButton.setVisibility(View.VISIBLE);
+            numberOfValuesRegistred = 0;
+            resultTextView.setText("");
+            resultTextView.setMaxLines(solvedPowerOfTwo);
+            resultTextView.setMovementMethod(new ScrollingMovementMethod());
+            System.out.println("Starting light sensor with fr "+friquancy+" and collecting "+solvedPowerOfTwo+" values...");
+            sensorManager.registerListener(this, lightSensor, timeInterval);
+        }
     }
 
     private void stopSensing(View v){
@@ -70,11 +127,20 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public final void onAccuracyChanged(Sensor sensor, int accuracy) {
         // Do something here if sensor accuracy changes.
+        System.out.println("Sensor: "+sensor.getName()+" has changed its accuracy to "+accuracy);
+        resultTextView.setText(resultTextView.getText()+"Смена точности на "+accuracy+" для "+sensor.getName()+"\n");
     }
 
     @Override
     public final void onSensorChanged(SensorEvent event) {
         float lightValue = event.values[0];
-        System.out.println("Light value: "+lightValue);
+        //System.out.println("Light value: "+lightValue);
+        if(numberOfValuesRegistred <= solvedPowerOfTwo){
+            String addintionStr = Float.valueOf(event.values[0]).toString()+"\n";
+            resultTextView.setText(resultTextView.getText()+addintionStr);
+            ++numberOfValuesRegistred;
+        }else{
+            stopSensing(null);
+        }
     }
 }
